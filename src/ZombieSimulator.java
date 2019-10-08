f0efa7fa90855ac2372953b9688d20d101774ae3
Moises <mdaboin@wustl.edu>
2019-10-08 11:18:20 -0500

    zombies AGHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- src/ZombieSimulator.java
+++ src/ZombieSimulator.java
@@ -1,6 +1,7 @@
 import edu.princeton.cs.introcs.StdDraw;
 import edu.princeton.cs.introcs.In;
 import support.cse131.ArgsProcessor;
+
 import java.awt.*;
 
 
@@ -19,7 +20,7 @@ public class ZombieSimulator {
 
     /**
      * Read entities from a file and return an array of Entities.
-     *
+     * <p>
      * Each row of the returned array represents one entity.  Column [TYPE] indicates the type
      * of entity (1 or ZOMBIE for zombies, 2 or NONZOMBIE for non-zombies),
      * Column [X] is the x coordinate and Column [Y] is the y coordinate.
@@ -28,13 +29,16 @@ public class ZombieSimulator {
      * @return a 2D array of entities.  Each row corresponds to an entity
      */
     public static double[][] readEntities(String fileName) {
-
-        // TODO: Implement this function (you can probably adjust code from Lab 4)
-
-        // Return a 2D array of entities
-        return null;  // FIXME: Replace this: Return the array of entities
+        In inFile = new In(fileName);
+        int numEntities = inFile.readInt();
+        double[][] entities = new double[numEntities][3];
+        for (int i = 0; i < entities.length; ++i) {
+            entities[i][TYPE] = inFile.readInt();
+            entities[i][X] = inFile.readDouble();
+            entities[i][Y] = inFile.readDouble();
+        }
+        return entities;
     }
-
     /**
      * Draw all the entities.  Zombies are drawn with filled red filled circles of radius 0.005 and
      * non-zombies with filled black filled circles of radius 0.005)
@@ -44,68 +48,90 @@ public class ZombieSimulator {
     public static void drawEntities(double[][] entities) {
         // DONE: Clear the frame
         StdDraw.clear();
-
-        // TODO: Write the loop that displays all the entities (you can probably adjust code from Lab 4)
-
-
-        // DONE: Show everything that was drawn (show the updated frame).  This should be the only "show()" command!
+        for (int i = 0; i < entities.length; ++i) {
+            if (entities[i][TYPE] == 1) {
+                StdDraw.setPenColor(Color.RED);
+                StdDraw.filledCircle(entities[i][X], entities[i][Y], 0.005);
+            } else if (entities[i][TYPE] == 2) {
+                StdDraw.setPenColor(Color.BLACK);
+                StdDraw.filledCircle(entities[i][X], entities[i][Y], 0.005);
+            }
+        }
         StdDraw.show();
     }
-
-
     /**
      * Check if the entity at the given index is touching a zombie.
-     * (HINT: You know the location of the center of each entity and 
-     *        that they all have a radius of 0.005.  If the circles
-     *        representing two entities overlap the are considered to be 
-     *        touching.  Consider using the distance formula)
+     * (HINT: You know the location of the center of each entity and
+     * that they all have a radius of 0.005.  If the circles
+     * representing two entities overlap the are considered to be
+     * touching.  Consider using the distance formula)
      *
-     * @param index the index of the entity to check
+     * @param index    the index of the entity to check
      * @param entities a 2D array of entities.  Each row corresponds to an entity.
      * @return true if the entity at index is touching a zombie
      */
     public static boolean touchingZombie(int index, double[][] entities) {
-        // TODO: Complete this method 
-
-        return false; // FIXME: Replace this so it returns the value of interest
+        for (int i = 0; i < entities.length; ++i) {
+            double x1 = entities[index][X];
+            double y1 = entities[index][Y];
+            if (entities[i][TYPE] == 1) {
+                double x2 = entities[i][X];
+                double y2 = entities[i][Y];
+                double xDifference = Math.pow((x2 - x1), 2.0);
+                double yDifference = Math.pow((y2 - y1), 2.0);
+                double totDistance = Math.sqrt(xDifference + yDifference);
+                if (totDistance <= 0.01) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
-
     /**
      * Update the locations of all entities (assume brownian motion)
-     *
+     * <p>
      * The rules for an update are:
-     *     Each entity should move by a random value between -0.005 and 0.005 in the x coordinate
-     *     and between -0.005 and 0.005 in the Y coordinate.
-     *    
-     *     Entities should not be able to leave the screen. x and y coordinates should be kept between [0-1.0]
-     *
-     *     If a non-zombie is touching a zombie it should change to a zombie.
-     *     (HINT: you need to check all entities. On each one that is NOT a zombie, you can re-use code
-     *            you've already written to see if it's "touching" a Zombie and, if so, change it to a zombie.)
+     * Each entity should move by a random value between -0.005 and 0.005 in the x coordinate
+     * and between -0.005 and 0.005 in the Y coordinate.
+     * <p>
+     * Entities should not be able to leave the screen. x and y coordinates should be kept between [0-1.0]
+     * <p>
+     * If a non-zombie is touching a zombie it should change to a zombie.
+     * (HINT: you need to check all entities. On each one that is NOT a zombie, you can re-use code
+     * you've already written to see if it's "touching" a Zombie and, if so, change it to a zombie.)
      *
      * @param entities a 2D array of entities.  Each row corresponds to an entity.
      */
     public static void updateEntities(double[][] entities) {
-        // TODO: Complete this method: It should update the positions of items in the entities array
-
-
-        // Note: No return value.  This "returns" when it hits the end }.
+        for (int i = 0; i < entities.length; ++i) {
+            double newX = entities[i][X] + ((Math.random() * 0.01)-0.005);
+            double newY = entities[i][Y] + ((Math.random() * 0.01)-0.005);
+            if(newX>=0 && newX<=1 ){
+                entities[i][X] = newX;
+            }
+            if(newY>=0 && newY<=1){
+                entities[i][Y] = newY;
+            }
+            if(touchingZombie(i, entities) && entities[i][TYPE]==NONZOMBIE){
+                entities[i][TYPE] = ZOMBIE;
+            }
+        }
     }
-
-
     /**
      * Return the number of nonzombies remaining
      *
-     * @param entities  a 2D array of entities.  Each row corresponds to an entity.
-     * @return TODO: This should briefly explain what the returned value means
+     * @param entities a 2D array of entities.  Each row corresponds to an entity.
+     * @return
      */
-    // TODO: Figure out the types to use below and uncomment the line (and the line for the return and ending } )
-//    public static TYPE nonzombieCount(TYPE entities) {
-//    // TODO: complete this method
-//
-//        return 0; // FIXME: Replace this so it returns the value of interest
-//     }
-
+    public static int nonzombieCount(double [][]entities) {
+        int nonZombie = 0;
+        for (int i = 0; i < entities.length; ++i) {
+            if (entities[i][TYPE] == NONZOMBIE) {
+                nonZombie = nonZombie + 1;
+            }
+        }
+        return nonZombie;
+    }
 
     public static void main(String[] args) {
         StdDraw.enableDoubleBuffering();   // Enable double buffering
@@ -118,10 +144,9 @@ public class ZombieSimulator {
         drawEntities(entities);
         StdDraw.pause(1000);
 
-        // TODO: Write the loop that will run the simulation.
-        //   Continue if there are nonzombies
-        //   Update positions
-        //   Redraw things
+        while(nonzombieCount(entities)!=0){
+            updateEntities(entities);
+            drawEntities(entities);
+        }
     }
-
 }
--- src/nonzombieCountTests.java
+++ src/nonzombieCountTests.java
@@ -11,70 +11,70 @@ import static org.junit.Assert.*;
 public class nonzombieCountTests {
 
 
-    // /**
-    //  * Test a single nonzombie (and a single entity)
-    //  */
-    // @Test
-    // public void testSingleEntitySingleSurvivor() {
-    //     double[][] entities = {{ 2, 0.5, 0.5 }};
-    //     assertEquals(1, ZombieSimulator.nonzombieCount(entities));
-    // }
+     /**
+      * Test a single nonzombie (and a single entity)
+      */
+     @Test
+     public void testSingleEntitySingleSurvivor() {
+         double[][] entities = {{ 2, 0.5, 0.5 }};
+         assertEquals(1, ZombieSimulator.nonzombieCount(entities));
+     }
 
-    // /**
-    //  * Test a single zombie (and a single entity)
-    //  */
-    // @Test
-    // public void testSingleEntityNoSurvivor() {
-    //     double[][] entities = {{ 1, 0.5, 0.5 }};
-    //     assertEquals(0, ZombieSimulator.nonzombieCount(entities));
-    // }
+     /**
+      * Test a single zombie (and a single entity)
+      */
+     @Test
+     public void testSingleEntityNoSurvivor() {
+         double[][] entities = {{ 1, 0.5, 0.5 }};
+         assertEquals(0, ZombieSimulator.nonzombieCount(entities));
+     }
 
-    // /**
-    //  * Test two entities and no nonzombies
-    //  */
-    // @Test
-    // public void testTwoEntitiesTwoSurvivors() {
-    //     double[][] entities = {{ 1, 0.5, 0.5 }, { 1, 0.75, 0.5 }};
-    //     assertEquals(0, ZombieSimulator.nonzombieCount(entities));
-    // }
+     /**
+      * Test two entities and no nonzombies
+      */
+     @Test
+     public void testTwoEntitiesTwoSurvivors() {
+         double[][] entities = {{ 1, 0.5, 0.5 }, { 1, 0.75, 0.5 }};
+         assertEquals(0, ZombieSimulator.nonzombieCount(entities));
+     }
 
 
-    // /**
-    //  * Test two entities and two nonzombies
-    //  */
-    // @Test
-    // public void testTwoEntitiesNoSurvivors() {
-    //     double[][] entities = {{ 2, 0.5, 0.5 }, { 2, 0.75, 0.5 }};
-    //     assertEquals(2, ZombieSimulator.nonzombieCount(entities));
-    // }
+     /**
+      * Test two entities and two nonzombies
+      */
+     @Test
+     public void testTwoEntitiesNoSurvivors() {
+         double[][] entities = {{ 2, 0.5, 0.5 }, { 2, 0.75, 0.5 }};
+         assertEquals(2, ZombieSimulator.nonzombieCount(entities));
+     }
 
-    // /**
-    //  * Test two entities and one nonzombie
-    //  */
-    // @Test
-    // public void testTwoEntitiesFirstSurvives() {
-    //     double[][] entities = {{ 2, 0.5, 0.5 }, { 1, 0.75, 0.5 }};
-    //     assertEquals(1, ZombieSimulator.nonzombieCount(entities));
-    // }
+     /**
+      * Test two entities and one nonzombie
+      */
+     @Test
+     public void testTwoEntitiesFirstSurvives() {
+         double[][] entities = {{ 2, 0.5, 0.5 }, { 1, 0.75, 0.5 }};
+         assertEquals(1, ZombieSimulator.nonzombieCount(entities));
+     }
 
-    // /**
-    //  * Test two entities and one nonzombie
-    //  */
-    // @Test
-    // public void testTwoEntitiesSecondSurvives() {
-    //     double[][] entities = {{ 1, 0.5, 0.5 }, { 2, 0.75, 0.5 }};
-    //     assertEquals(1, ZombieSimulator.nonzombieCount(entities));
-    // }
+     /**
+      * Test two entities and one nonzombie
+      */
+     @Test
+     public void testTwoEntitiesSecondSurvives() {
+         double[][] entities = {{ 1, 0.5, 0.5 }, { 2, 0.75, 0.5 }};
+         assertEquals(1, ZombieSimulator.nonzombieCount(entities));
+     }
 
-    // /**
-    //  * Test 100 entities and 50 nonzombies
-    //  */
-    // @Test
-    // public void testOneHundredEntitiesFiftySurvive() {
-    //     double[][] entities = new double[100][3];
-    //     for(int i=0;i<entities.length;i++) {
-    //         entities[i][0] = i%2==0 ? 1 : 2;
-    //     }
-    //     assertEquals(50, ZombieSimulator.nonzombieCount(entities));
-    // }
+     /**
+      * Test 100 entities and 50 nonzombies
+      */
+     @Test
+     public void testOneHundredEntitiesFiftySurvive() {
+         double[][] entities = new double[100][3];
+         for(int i=0;i<entities.length;i++) {
+             entities[i][0] = i%2==0 ? 1 : 2;
+         }
+         assertEquals(50, ZombieSimulator.nonzombieCount(entities));
+     }
 }
